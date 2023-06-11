b585d79ee3b2d7c3da03f2197fec813f0c768255
Willian Pinheiro <willpinhaaa@gmail.com>
2023-06-11 18:57:31 -0300

    Adiciona esqueleto da página

--- src/App.svelte
+++ src/App.svelte
@@ -6,27 +6,53 @@
 	.container {
 		display: flex;
 		min-height: 100vh;
+		flex-direction: column;
+		align-items: center;
+		gap: 2rem; 
 	}
 
 	.menu-panel {
 		display: flex;
+		gap: 1rem; 
 	}
 
 	.chat-panel {
 		display: flex;
+		flex-direction: column;
+		align-items: start;
+		gap: 1rem;
+		width: 50%;
+		padding: 1rem;
+		border: 1px solid black;
+		border-radius: 0.25rem;
+	}
+
+	.menu-button {
+		padding: 1rem;
+		border: 1px solid black;
+		border-radius: 0.25rem;
+	}
+
+	.message {
+		padding: 1rem;
+		border: 1px solid black;
+		border-radius: 0.25rem;
 	}
 </style>
 
 <div class="container">
 	<div class="menu-panel">
-		<div>A</div>
-		<div>A</div>
-		<div>A</div>
+		<div class="menu-button">Chat</div>
+		<div class="menu-button">Pessoas Online</div>
 	</div>
 
 	<div class="chat-panel">
-		<div>B</div>
-		<div>B</div>
-		<div>B</div>
+		<div class="message">
+			Essa é uma mensagem doida
+		</div>
+
+		<div class="message">
+			Essa é uma mensagem doida
+		</div>
 	</div>
 </div>
