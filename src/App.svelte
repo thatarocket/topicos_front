c5537d61dcf6256111c37126e96679aa11021263
Willian Pinheiro <willpinhaaa@gmail.com>
2023-06-28 19:05:47 -0300

    Add beautiful login page

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,12 @@
 <script>
-	import { connect } from "./utils/chat";
-	import { isConnected, username } from "./stores/chat";
+	import Login from "./lib/Login.svelte";
 	import UserPanel from "./lib/UserPanel.svelte";
 
-	let usernameInput = "";
-
-	function handleClick() {
-		connect(usernameInput);
-		username.set(usernameInput);
-	}
+	import { isConnected } from "./stores/chat";
 </script>
 
 {#if $isConnected}
 	<UserPanel />
 {:else}
-	<div class="flex flex-col gap-2 border-2 p-8">
-		<input bind:value={usernameInput} placeholder="Enter a username..." />
-
-		<button on:click={handleClick}>Enter</button>
-	</div>
+	<Login />
 {/if}
---
+++ src/lib/Login.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { connect } from "../utils/chat";
+	import { username } from "../stores/chat";
+
+	let usernameInput = "";
+
+	function login() {
+		connect(usernameInput);
+		username.set(usernameInput);
+	}
+</script>
+
+<div
+	class="flex min-h-screen flex-col items-center justify-center gap-5 bg-gradient-to-l from-gray-900 to-gray-950 text-white"
+>
+	<div
+		class="flex flex-col items-center gap-6 rounded-lg border bg-slate-800 p-4"
+	>
+		<div class="font-semibold">🛸 SocketChat</div>
+
+		<input
+			class="rounded border bg-transparent p-2"
+			bind:value={usernameInput}
+			placeholder="Insira um nome de usuário..."
+		/>
+
+		<button
+			class="rounded bg-slate-300 px-2 py-1 text-slate-950 hover:bg-slate-400"
+			on:click={login}>💬 Entrar</button
+		>
+	</div>
+
+	<div class="text-xs">
+		Desenvolvido com ❤️ por Gabriela, Silas, Thais e Willian
+	</div>
+</div>
